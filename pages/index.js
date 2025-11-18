20d3f2d49320899cedc13b185eb1905106cd4df6
Bruna B Moreira <jgdbruna@gmail.com>
2025-11-18 20:45:29 +0000

    ajuste na mensagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 function Home() {
-    return <h1>Segure firme, seja forte e seja verdadeira consigo mesma. Tudo irá conspirar ao seu favor. Você não está atrasada nem adiantada, você está exatamente na hora certa!</h1>
+    return <h1>Que esta frase possa lhe trazer calma e um sorriso no rosto:
+        "Segure firme, seja forte e seja verdadeira consigo mesma.
+         Tudo irá conspirar ao seu favor. Você não está atrasada nem adiantada, 
+         você está exatamente na hora certa!" por Gurudev </h1>
 }
 
 export default Home;
