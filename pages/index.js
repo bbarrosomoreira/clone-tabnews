bc23bc99f0adfb72514c79146878d7d136c98310
Bruna B Moreira <jgdbruna@gmail.com>
2025-11-18 20:02:55 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Você não está atrasada nem adiantada, você está exatamente na hora certa!</h1>
+    return <h1>Segure firme, seja forte e seja verdadeira consigo mesma. Tudo irá conspirar ao seu favor. Você não está atrasada nem adiantada, você está exatamente na hora certa!</h1>
 }
 
 export default Home;
